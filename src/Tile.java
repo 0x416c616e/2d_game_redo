8f10b2dc504e56e2114a79af86fd5d450efad2a5
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-12-11 21:14:43 -0600

    made empty Event class and basic Tile class

---
+++ src/Tile.java
@@ -0,0 +1,9 @@
+public class Tile {
+    Event event;
+    String topLevel; //decoration
+    String midLevel;
+    String bottomLevel;
+    boolean collision; //true means you can't pass through it
+
+
+}
